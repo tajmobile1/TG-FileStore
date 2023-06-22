bbf5606da217e3ca04860282c747fbf7016689ff
tajmobile1 <114266082+tajmobile1@users.noreply.github.com>
2023-06-22 14:28:05 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,15 +3,15 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	API_ID = int(os.environ.get("API_ID", "9738903"))
+	API_HASH = os.environ.get("API_HASH", "d05599b2b23fd03e208ca54a2ff93445")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5994563810:AAFRy9X_0NK2YHRrsBZoMYeGnpCZSlpMVF0")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "tajlinkfilestore_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001648402512"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1030188110"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://tgfilestore:tgfilestore@cluster0.ippxmby.mongodb.net/?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001256648377")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001871743751")
 	HOME_TEXT = """
 Hello [{}](tg://user?id={}), I am a file or media store bot. Send me any file or media I will give you a permanent sharable link. I support channel also.
 
